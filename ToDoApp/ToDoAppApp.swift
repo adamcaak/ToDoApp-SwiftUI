6c048651988db3a4ef286e098d9f42c9d7d69ae9
adamcaak <ladamczak1997@gmail.com>
2025-04-29 14:07:05 +0200

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Łukasz Adamczak on 29/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
